1a2170a7157fb02d4dc0f877519ca3cd327f1eed
Let-EM <leticia.montemor@estantemagica.com.br>
2025-07-07 20:11:05 -0300

    Add files via upload

---
+++ WA_Logo_Long.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="WA_Logo_Long" tilewidth="32" tileheight="32" tilecount="6" columns="6">
+ <image source="map-starter-kit-master/tilesets/WA_Logo_Long.png" width="192" height="32"/>
+</tileset>
